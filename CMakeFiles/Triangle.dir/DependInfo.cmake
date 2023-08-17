f0815b20a467856740ce8fe5349b071bcf1d9f55
Jasper <jasperris93@gmail.com>
2023-08-17 22:56:54 +0200

    trying cmake

---
+++ CMakeFiles/Triangle.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/dev/vulkan/mastering-graphics-programming/triangle/debugshit.cpp" "CMakeFiles/Triangle.dir/debugshit.cpp.o" "gcc" "CMakeFiles/Triangle.dir/debugshit.cpp.o.d"
+  "/mnt/dev/vulkan/mastering-graphics-programming/triangle/main.cpp" "CMakeFiles/Triangle.dir/main.cpp.o" "gcc" "CMakeFiles/Triangle.dir/main.cpp.o.d"
+  "/mnt/dev/vulkan/mastering-graphics-programming/triangle/p_device.cpp" "CMakeFiles/Triangle.dir/p_device.cpp.o" "gcc" "CMakeFiles/Triangle.dir/p_device.cpp.o.d"
+  "/mnt/dev/vulkan/mastering-graphics-programming/triangle/presentation.cpp" "CMakeFiles/Triangle.dir/presentation.cpp.o" "gcc" "CMakeFiles/Triangle.dir/presentation.cpp.o.d"
+  "/mnt/dev/vulkan/mastering-graphics-programming/triangle/requirement.cpp" "CMakeFiles/Triangle.dir/requirement.cpp.o" "gcc" "CMakeFiles/Triangle.dir/requirement.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Triangle.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Triangle.dir/debugshit.cpp.o"
+  "CMakeFiles/Triangle.dir/debugshit.cpp.o.d"
+  "CMakeFiles/Triangle.dir/main.cpp.o"
+  "CMakeFiles/Triangle.dir/main.cpp.o.d"
+  "CMakeFiles/Triangle.dir/p_device.cpp.o"
+  "CMakeFiles/Triangle.dir/p_device.cpp.o.d"
+  "CMakeFiles/Triangle.dir/presentation.cpp.o"
+  "CMakeFiles/Triangle.dir/presentation.cpp.o.d"
+  "CMakeFiles/Triangle.dir/requirement.cpp.o"
+  "CMakeFiles/Triangle.dir/requirement.cpp.o.d"
+  "Triangle"
+  "Triangle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Triangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
