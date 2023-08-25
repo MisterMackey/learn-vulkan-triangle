a920d2f341f57294f37fb4f5a42e4c91406787f6
Jasper <jasperris93@gmail.com>
2023-08-25 22:05:09 +0200

    made render pass

--- main.cpp
+++ main.cpp
@@ -43,6 +43,7 @@ class TriangleApp{
 	VkQueue presentQueue;
 	trianglePresentation::swapchainInformation swapchainInfo;
 	std::vector<VkImageView> swapChainImageViews;
+	VkRenderPass renderPass;
 	VkPipelineLayout pipelineLayout;
 
 	void initWindow(void)
@@ -65,6 +66,7 @@ class TriangleApp{
 		createImageViews();
 		//im giving up on splitting all this shit up into files. I don't know enough to properly factor this shit anyway.
 		//so im just following the tutorial now
+		createRenderPass();
 		createGraphicsPipeline();
 
 	}
@@ -77,6 +79,7 @@ class TriangleApp{
 	void  cleanup(void)
 	{
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+		vkDestroyRenderPass(device, renderPass, nullptr);
 		for (auto imageView : swapChainImageViews){
 			vkDestroyImageView(device, imageView, nullptr);
 		}
@@ -281,6 +284,7 @@ class TriangleApp{
 		pipelineLayoutCreateInfo.pSetLayouts = nullptr;
 		pipelineLayoutCreateInfo.pushConstantRangeCount = 0;
 		pipelineLayoutCreateInfo.pPushConstantRanges = nullptr;
+		pipelineLayoutCreateInfo.pNext = nullptr; //based on validation layer output
 
 		if (vkCreatePipelineLayout(device, &pipelineLayoutCreateInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
 			throw std::runtime_error("Failed to make pipeline layout");
@@ -289,6 +293,39 @@ class TriangleApp{
 		//wut?
 		vkDestroyShaderModule(device, fragShaderModule, nullptr);
 		vkDestroyShaderModule(device, vertexShaderModule, nullptr);
+	}
+
+	void createRenderPass(void)
+	{
+		VkAttachmentDescription colorAttachment{};
+		colorAttachment.format = swapchainInfo.swapchainImageFormat;
+		colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+		VkAttachmentReference colorAttachmentRef{};
+		colorAttachmentRef.attachment = 0;
+		colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+		VkSubpassDescription subpass {};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &colorAttachmentRef;
+
+		VkRenderPassCreateInfo renderPassInfo {};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		renderPassInfo.attachmentCount = 1;
+		renderPassInfo.pAttachments = &colorAttachment;
+		renderPassInfo.subpassCount = 1;
+		renderPassInfo.pSubpasses = &subpass;
+
+		if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS){
+			throw std::runtime_error("Failed to create render pass");
+		}
 	}
 };
 
